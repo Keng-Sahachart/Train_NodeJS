35616293f0ed01e7d4525fc371a49b238b429cdd
Sahachart Panlawan <55072247+Keng-Sahachart@users.noreply.github.com>
2020-09-01 07:19:34 +0700

    blog workshop ep1-2

---
+++ KRS03-BlogAppication-workshop/routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/** database */
+var mongodb = require('mongodb');
+var db = require('monk')('localhost:27017');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+  var blogs = db.get('posts');  // กำหนด table
+  blogs.find({}, {},function(err, blog){    // เอาทุก row
+    res.render('index', { posts: 'blog' });   // ???????????????
+  });
+
+
+
+  // res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
